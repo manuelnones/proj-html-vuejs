29d8b26606d8d5e42ae336ba9db5219180a66b3c
manuelnones <manuelnones99@yahoo.com>
2023-04-13 12:53:05 +0200

    structure footer

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
 
 export default {
   data() {
@@ -12,6 +13,7 @@ export default {
   components: {
     AppHeader,
     AppMain,
+    AppFooter,
   }
 }
 </script>
@@ -19,6 +21,7 @@ export default {
 <template>
   <AppHeader></AppHeader>
   <AppMain></AppMain>
+  <AppFooter></AppFooter>
 </template>
 
 <style lang="scss"></style>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,75 @@
+<script>
+export default {
+    name: `AppFooter`,
+    data() {
+        return {
+            addressLinks: [
+                `382 NE 191st St # 87394 Miami, FL 33179-3899`,
+                `+1 (305) 547-9909 (9am - 5pm EST, Monday - Friday)`,
+                `support@maxcoach.com`,
+            ],
+
+            exploreLinks: [
+                `Start here`,
+                `Blog`,
+                `About us`,
+                `Success story`,
+                `Courses`,
+                `Contact us`,
+            ],
+
+            informationLinks: [
+                `Memebership`,
+                `Purchase guide`,
+                `Privacy policy`,
+                `Terms of services`,
+            ]
+        }
+    }
+}
+</script>
+
+<template>
+    <footer>
+
+        <div id="footer-container">
+            <div id="footer-left">
+
+            </div>
+
+            <div id="footer-right">
+
+            </div>
+        </div>
+
+    </footer>
+</template>
+
+<style lang="scss" scoped>
+footer {
+    background-color: #F8F8F8;
+
+    #footer-container {
+        height: 400px;
+        background-color: red;
+        // /prova 
+        display: flex;
+        justify-content: space-between;
+        width: 60%;
+        margin: auto;
+        padding-top: 60px;
+
+        #footer-left {
+            width: 250px;
+            height: 300px;
+            background-color: blue;
+        }
+
+        #footer-right {
+            width: 400px;
+            height: 300px;
+            background-color: orange;
+        }
+    }
+}
+</style>
